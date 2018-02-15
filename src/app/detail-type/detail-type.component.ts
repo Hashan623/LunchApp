c335980832e417a4282a6fb8d1b3698b39975544
Dasun Hasantha <dasun@friendtho.com>
2018-02-15 13:15:17 +0530

    Dropdown filtration

--- src/app/detail-type/detail-type.component.ts
+++ src/app/detail-type/detail-type.component.ts
@@ -1,3 +1,4 @@
+import { filter } from 'rxjs/operator/filter';
 import { UUID } from 'angular2-uuid';
 import { FoodDetail } from './../models/fooddetail';
 import { FoodType } from './../models/foodtype';
@@ -16,28 +17,26 @@ import 'rxjs/add/operator/map';
   selector: 'app-detail-type',
   templateUrl: './detail-type.component.html',
   styleUrls: ['./detail-type.component.css'],
+  //providers : [XxxService],
 })
 export class DetailTypeComponent implements OnInit{
   someData$;
   foodtypes$;
   selected;
-  selectedData$;
+  selectedData$ : any;
   foodDetails : FoodDetail[];
+  _xxServices : XxxService;
 
-  constructor(private _foodTypeService: FoodtypeService,_xxService: XxxService) 
+  constructor(private _foodTypeService: FoodtypeService, _xxService: XxxService) 
   { 
     this.foodtypes$ = _foodTypeService.getFoodtypesList();
-    this.selectedData$ = _xxService.getFooddetailsList();
-    //var data = this.selectedData$.val();
+    this._xxServices = _xxService;
+
     console.log(this.selectedData$);
   }
 
   onSelect(selected){
-    console.log(selected);
-    console.log(this.selectedData$);
-    this.selectedData$ = this.selectedData$.filter(x => x.foodType == selected);
-    console.log(this.selectedData$);
-    //this.selectedData$ = _xxService.getFooddetailsList();
+    this.selectedData$ = this._xxServices.getFooddetailsList(selected);
   }
 
   ngOnInit(){}
--- src/app/xxx.service.ts
+++ src/app/xxx.service.ts
@@ -1,18 +1,22 @@
 import { Injectable } from '@angular/core';
 import {AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable} from 'angularfire2/database';
 import { UUID } from 'angular2-uuid';
+import { filter } from 'rxjs/operator/filter';
 
 @Injectable()
 export class XxxService {
 
+
+
   constructor(private db: AngularFireDatabase) { }
 
 
-  getFooddetailsList() { 
-    return this.db.object('/fooddetails', {
-      // query: {
-      //   orderByChild: 'name'
-      // }
+  getFooddetailsList(value:string) { 
+    return this.db.list('/fooddetails', {
+       query: {
+         orderByChild: 'foodType',
+         equalTo : value
+       }
     });
   }
 
